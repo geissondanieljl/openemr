31b0172db657245a7c14aaa11f914d7e50428232
danieljl05 <geisondanieljl@gmail.com>
2019-08-14 17:04:29 -0500

    changes

--- setup.php
+++ setup.php
@@ -230,7 +230,7 @@ if (file_exists($OE_SITE_DIR)) {
 ?>
 <html>
 <head>
-<title>OpenEMR Setup Tool</title>
+<title>Daniel OpenEMR Setup Tool</title>
 <!--<link rel=stylesheet href="interface/themes/style_blue.css">-->
 <link rel="stylesheet" href="public/assets/bootstrap/dist/css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="public/assets/jquery-ui/jquery-ui.css" type="text/css">

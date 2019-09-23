b886abb818d8579e6060234c20f2578720143c79
Daniel Jiménez <geisondanieljl@gmail.com>
2019-09-23 17:20:24 -0500

    change

--- src/Services/AppointmentService.php
+++ src/Services/AppointmentService.php
@@ -233,7 +233,11 @@ class AppointmentService
           $index = 'successful';
           sqlStatement($sql);
         }
-        $rSchedule[$index][] = $idAppt;
+        $rSchedule[$index][$idAppt] = array(
+          'eDate' => $eDate,
+          'sTime' => $sTime,
+          'eTime' => $eTime
+        );
       }
     }
     return $rSchedule;

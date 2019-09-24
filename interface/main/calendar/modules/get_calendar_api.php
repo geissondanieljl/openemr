a0f4432d0064e36467232a5c76f8f7c5b46478dc
Daniel Jiménez <geisondanieljl@gmail.com>
2019-09-23 19:42:50 -0500

    test

--- interface/main/calendar/modules/get_calendar_api.php
+++ interface/main/calendar/modules/get_calendar_api.php
@@ -100,8 +100,8 @@ $slotbase  = (int) ($slotstime / $slotsecs);
 $slotcount = (int) ($slotetime / $slotsecs) - $slotbase;
 
 if ($slotcount <= 0 || $slotcount > 100000) {
-    // die(xlt("Invalid date range"));
-    die($slotcount);
+    die(xlt("Invalid date range"));
+    
 }
 
 $slotsperday = (int) (60 * 60 * 24 / $slotsecs);

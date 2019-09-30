29c65438968b3a9de3b7ea9f148332262090e7dd
Daniel Jiménez <geisondanieljl@gmail.com>
2019-09-30 10:58:00 -0500

    test

--- src/Services/ProviderService.php
+++ src/Services/ProviderService.php
@@ -103,7 +103,6 @@ class ProviderService
             array_push($arrayParams, $idAppt);
         }
         $sql .= " ORDER BY ope.pc_eventDate, ope.pc_startTime ";
-        return array('sql' => $sql, 'res' => $arrayParams);
         $statementResults = sqlStatement($sql, $arrayParams);
         $results = array();
         while ($row = sqlFetchArray($statementResults)) {
